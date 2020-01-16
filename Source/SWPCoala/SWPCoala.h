02c4ee222c0e9a9b2d3702b7e93176c4b1adcb60
Pranav <pranavpaharia@gmail.com>
2020-01-16 19:32:56 +0530

    Added Enums
    - EPlayerDirEnum
    - EChooseCity
    - EMapExpansion

--- Source/SWPCoala/SWPCoala.h
+++ Source/SWPCoala/SWPCoala.h
@@ -5,3 +5,33 @@
 #include "CoreMinimal.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogFlying, Log, All);
+
+
+UENUM(BlueprintType)
+enum class EPlayerDirEnum : uint8
+{
+	PD_North UMETA(DisplayName = "Player North"),
+	PD_East UMETA(DisplayName = "Player East"),
+	PD_South UMETA(DisplayName = "Player South"),
+	PD_West UMETA(DisplayName = "Player West")
+};
+
+UENUM(BlueprintType)
+enum class EChooseCity : uint8
+{
+	LondonBridge,
+	StatueOfLiberty,
+	GatewayOfIndia,
+	Dubai,
+	None
+};
+
+UENUM(BlueprintType)
+enum class EMapExpansion : uint8
+{
+	NorthExp,
+	SouthExp,
+	EastExp,
+	WestExp,
+	None
+};
