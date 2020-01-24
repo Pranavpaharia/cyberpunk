f54fe58bfe91b4c7ef17b8c87ce79bafdeefbf7d
Pranav <pranavpaharia@gmail.com>
2020-01-24 17:23:58 +0530

    Added work set

---
+++ Source/SWPCoala/Public/SWPGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SWPGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWPCOALA_API USWPGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
